d1a048ba4f19add72237808ed2c574c0c3b5d49a
eraime906 <eraime906@fareham.ac.uk>
2023-03-13 10:46:56 +0000

    Responsive nav-bar component complete

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -14,24 +14,33 @@ export default function NavbarComponent() {
     }
 
     return (
-        <div className={"h-16 w-full flex items-center "}>
-            <img
-                src={require("../assets/logo.png")}
-                alt={"Health Advice Group Logo"}
-                className={"h-12 w-12 m-4"}
-                onClick={() => onClick("")}
-            />
-            <button className={"text-2xl text-blue-800 font-mono font-bold"} onClick={() => onClick("")}>
-                Health Advice Group
-            </button>
-            <div className={"text-blue-800 flex justify-end"}>
-            {[
-                "education",
-                "forecasting",
-                "risks",
-                "about",
-                "login"
-            ].map(page => <button className={"m-4 text-xl font-mono hover:text-bold hover:underline"} onClick={() => onClick(page)}>{format(page)}</button>)}
+        <div className={"h-16 w-full flex items-center justify-content"}>
+            <div className={"flex w-96"}>
+                <img
+                    src={require("../assets/logo.png")}
+                    alt={"Health Advice Group Logo"}
+                    className={"h-12 w-12 m-4"}
+                    onClick={() => onClick("")}
+                />
+                <button className={"md:text-l lg:text-2xl text-blue-800 font-mono font-bold"}
+                        onClick={() => onClick("")}>
+                    Health Advice Group
+                </button>
+            </div>
+            {/*Drop-down for small screens*/}
+            <div className={"visible xl:hidden w-9/12 flex justify-end pr-8"}>
+                <img className={"w-12 h-12"} src={require("../assets/navbar-burger-icon.png")} alt={"Drop down menu containing links to other pages of the website"}/>
+            </div>
+            {/* Desktop view*/}
+            <div className={"hidden xl:visible xl:w-9/12 xl:flex justify-end mr-12 text-blue-800"}>
+                {[
+                    "education",
+                    "forecasting",
+                    "risks",
+                    "about",
+                    "login"
+                ].map(page => <button className={"m-4 text-xl font-mono hover:text-bold hover:underline"}
+                                      onClick={() => onClick(page)}>{format(page)}</button>)}
             </div>
         </div>
     )
