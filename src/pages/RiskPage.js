50dd1f6410b15796edfec1eda3fef3dabf4e7b09
eraime906 <eraime906@fareham.ac.uk>
2023-03-22 11:35:41 +0000

    Risks page banner component

--- src/pages/RiskPage.js
+++ src/pages/RiskPage.js
@@ -1,10 +1,12 @@
 import NavbarComponent from "../components/NavbarComponent";
 import FooterComponent from "../components/FooterComponent";
+import BannerComponent from "../components/BannerComponent";
 
 export default function RiskPage() {
     return (
         <>
             <NavbarComponent />
+            <BannerComponent src={"risks-banner.jpg"} alt={"Image of a street name called time for change"}/>
             <FooterComponent />
         </>
     )
